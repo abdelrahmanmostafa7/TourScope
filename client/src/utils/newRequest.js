b1c7de686aa8ab45e788f6cdc84b0639e62358a9
anter0101 <abdullah.helal2000@gmail.com>
2023-06-26 05:17:26 +0300

    solve corsssssssssssssss

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,12 +1,20 @@
 import axios from "axios"
 
 export const newRequest = axios.create({
-    baseURL:"http://localhost:8800/api/",
-    withCredentials:true
-})
+    baseURL: "http://localhost:8800/api/",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+  });
+  
 
 export const Ai_api = axios.create({
     baseURL:"http://localhost:5000/",
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     withCredentials:true
 
 })
