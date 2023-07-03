482f2be93111f6551a17b04fbca7cb1022353d86
LordAhmed71 <78278325+LordAhmed71@users.noreply.github.com>
2023-07-03 16:18:09 +0300

    admin meddleware mapbox filter

---
+++ api/middleware/adminJwt.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
+
+export const checkRole = async (req, res, next) => {
+  const token = req.cookies.accessToken;
+  if (!token) return next(createError(401, "You are not authenticated!"));
+
+  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+    if (err) return next(createError(403, "Token is not valid!"));
+
+    if (payload.role !== "supervisor" && payload.role !== "moderator") {
+      res.status(200).send("You are not authenticated!");
+    }
+    next();
+  });
+};
+
+export const checkSupervisor = async (req, res, next) => {
+  const token = req.cookies.accessToken;
+  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+    if (err) return next(createError(403, "Token is not valid!"));
+
+    const role = payload.role;
+    if (role !== "supervisor") {
+      next(createError(403, "ٌRole is not Authorized!"));
+    }
+    next();
+  });
+};
+
+
+
+export const checkModerator = async (req, res, next) => {
+  const token = req.cookies.accessToken;
+
+  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+    if (err) return next(createError(403, "Token is not valid!"));
+
+    const role = payload.role;
+    if (role !== "moderator") {
+      next(createError(403, "ٌRole is not Authorized!"));
+    }
+    next();
+  });
+};
