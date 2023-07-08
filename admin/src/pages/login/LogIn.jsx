e3e43111cd0d46da2ae836735a93ef28abb481f4
LordAhmed71 <78278325+LordAhmed71@users.noreply.github.com>
2023-07-09 00:50:34 +0300

    des

--- admin/src/pages/login/LogIn.jsx
+++ admin/src/pages/login/LogIn.jsx
@@ -35,53 +35,6 @@ const LogInOut = () => {
 
   }, [error])
 
-  const closeBtn = () => {
-    navigate("/");
-  }
-
-
-  const inputs = [
-    {
-      id: 1,
-      name: "first_name",
-      type: "text",
-      errorMessage:
-        "Username should be 3-16 characters and shouldn't include any special character!",
-      label: "First Name",
-      pattern: "^[A-Za-z0-9]{3,16}$",
-      required: true,
-    },
-    {
-      id: 2,
-      name: "last_name",
-      type: "text",
-      errorMessage:
-        "Username should be 3-16 characters and shouldn't include any special character!",
-      label: "Last Name",
-      pattern: "^[A-Za-z0-9]{3,16}$",
-      required: true,
-    },
-    {
-      id: 3,
-      name: "email",
-      type: "email",
-      errorMessage: "It should be a valid email address!",
-      label: "Email",
-      required: true,
-    },
-    {
-      id: 4,
-      name: "password",
-      type: "password",
-      errorMessage:
-        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-      label: "Password",
-      pattern: "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$",
-      required: true,
-    },
-
-  ];
-
   return (
     <div className="loginContainer">
       {/* <img src={Close} alt="" className='closeBtn' onClick={closeBtn} /> */}
